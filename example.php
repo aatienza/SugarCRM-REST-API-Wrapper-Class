660e650dc1975b75ceeb6870927dc2f17a4ca78b
Alvaro Atienza <alv.atienza@gmail.com>
2016-02-17 11:09:00 -0500

    small tweaks

--- example.php
+++ example.php
@@ -1,4 +1,4 @@
-<!doctype html>  
+<!doctype html>
 
 	<head>
   		<meta charset="utf-8">
@@ -11,25 +11,25 @@
 				float:none;
 				clear:both;
 			}
-			
+
 			div {
 				padding:4px;
 				margin:4px;
 			}
-			
+
 			div.first {
 				border:1px solid #8F8F8F;
 				margin-bottom:20px;
 			}
-		
+
 			div.second {
 				border:1px solid #9F9F9F;
 			}
-			
+
 			div.third {
 				border:1px solid #CCC;
 			}
-			
+
 			div.fourth{
 				border:1px solid #CFCFCF;
 			}
@@ -39,11 +39,8 @@
 <body>
 
 <?php
-<<<<<<< HEAD
+
 $sugar = new \aatienza\SugarWrapper\Rest;
-=======
-$sugar = new \Asakusuma\SugarWrapper\Rest;
->>>>>>> cc0a320e14fae8ac3a38581b50d111ccc70fe861
 
 $sugar->setUrl('https://sugarcrm/service/v2/rest.php');
 $sugar->setUsername('user');
